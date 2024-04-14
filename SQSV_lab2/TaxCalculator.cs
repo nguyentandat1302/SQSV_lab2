da3608b2d92a5061a8f0253327fd0eab4fcc4ec7
datnguyen13021302 <21280103e0017@student.tdmu.edu.vn>
2024-04-14 10:25:18 +0700

    Add project files.

---
+++ SQSV_lab2/TaxCalculator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQSV_lab2
+{
+    public class TaxCalculator
+    {
+        public static float MONTHLY_REDUNDANT = 11000000;
+        public static float DEPENDENT_REDUNDANT = 4400000;
+
+        public static float Calculate(float income, float dependentQty)
+        {
+            float taxableIncome = income
+                - MONTHLY_REDUNDANT
+                - dependentQty * DEPENDENT_REDUNDANT;
+            float taxAmount = -1;
+
+            if (taxableIncome == 0) 
+            {
+                taxAmount = 0;
+            }
+            if (taxableIncome >= 0 && taxableIncome <= 5000000)
+            {
+                taxAmount = 0.005F * taxableIncome;
+            }
+            else if (taxableIncome <= 10000000)
+            {
+                taxAmount = 0.01F * taxableIncome - 250000;
+            }
+            else if (taxableIncome <= 18000000)
+            {
+                taxAmount = 0.01F * taxableIncome - 750000;
+            }
+            else if (taxableIncome <= 32000000)
+            {
+                taxAmount = 0.02F * taxableIncome - 1650000;
+            }
+            else if (taxableIncome <= 52000000)
+            {
+                taxAmount = 0.02F * taxableIncome - 3250000;
+            }
+            else if (taxableIncome <= 80000000)
+            {
+                taxAmount = 0.03F * taxableIncome - 5850000;
+            }
+            else
+            {
+                taxAmount = 0.035F * taxableIncome - 9850000;
+            }
+
+            return taxAmount;
+        }
+    }
+}
---
+++ TestLab2/UnitTest1.cs
@@ -0,0 +1,108 @@
+﻿using SQSV_lab2;
+using FluentAssertions;
+
+namespace TestLab2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            float income = 100000000; // Thu nhập cao
+            float dependentQty = 2;    
+            float expectedResult = 0.035F * (income - TaxCalculator.MONTHLY_REDUNDANT - dependentQty * TaxCalculator.DEPENDENT_REDUNDANT) - 9850000;
+
+           
+            float actualResult = TaxCalculator.Calculate(income, dependentQty);
+
+            
+            actualResult.Should().BeApproximately(expectedResult, 0.01f);
+        }
+
+        [TestMethod]
+        public void Calculate_TaxForNoDependent_ShouldBeCorrect()
+        {
+            // Arrange
+            float income = 20000000; //  Thu nhập trung bình
+            float dependentQty = 0;  
+            float expectedResult = 0.02F * (income - TaxCalculator.MONTHLY_REDUNDANT - dependentQty * TaxCalculator.DEPENDENT_REDUNDANT) - 3250000;
+
+            // Act
+            float actualResult = TaxCalculator.Calculate(income, dependentQty);
+
+            // Assert
+            actualResult.Should().BeApproximately(expectedResult, 0.01f);
+        }
+
+        [TestMethod]
+        public void Calculate_TaxForMultipleDependents_ShouldBeCorrect()
+        {
+            // Arrange
+            float income = 50000000; // Thu nhập trung bình cao
+            float dependentQty = 3;  
+            float expectedResult = 0.02F * (income - TaxCalculator.MONTHLY_REDUNDANT - dependentQty * TaxCalculator.DEPENDENT_REDUNDANT) - 3250000;
+
+            float actualResult = TaxCalculator.Calculate(income, dependentQty);
+
+            actualResult.Should().BeApproximately(expectedResult, 0.01f);
+        }
+
+        [TestMethod]
+        public void Calculate_TaxForZeroIncome_ShouldBeZero()
+        {
+            float income = 0;
+            float dependentQty = 1;
+            float expectedResult = 0;
+
+            float actualResult = TaxCalculator.Calculate(income, dependentQty);
+
+            actualResult.Should().BeApproximately(expectedResult, 0.01f);
+        }
+
+        [TestMethod]
+        public void Calculate_TaxForIncomeFromZeroToFiveMillion_ShouldBeCorrect()
+        {
+            // Arrange
+            float income = 3000000;
+            float dependentQty = 1;
+            float expectedResult = 0.005F * income;
+
+            // Act
+            float actualResult = TaxCalculator.Calculate(income, dependentQty);
+
+            // Assert
+            actualResult.Should().BeApproximately(expectedResult, 0.01f);
+        }
+
+
+        [TestMethod]
+        public void Calculate_TaxForIncomeFromFiveMillionToTenMillion_ShouldBeCorrect()
+        {
+            // Arrange
+            float income = 8000000;
+            float dependentQty = 1;
+            float expectedResult = 0.01F * income - 250000;
+
+            // Act
+            float actualResult = TaxCalculator.Calculate(income, dependentQty);
+
+            // Assert
+            actualResult.Should().BeApproximately(expectedResult, 0.01f);
+        }
+
+        [TestMethod]
+        public void Calculate_TaxForIncomeAboveEightyMillion_ShouldBeCorrect()
+        {
+            // Arrange
+            float income = 90000000;
+            float dependentQty = 1;
+            float expectedResult = 0.035F * income - 9850000;
+
+            float actualResult = TaxCalculator.Calculate(income, dependentQty);
+
+            actualResult.Should().BeApproximately(expectedResult, 0.01f);
+        }
+
+    }
+}
